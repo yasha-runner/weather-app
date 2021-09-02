fbba6c25e79b5a2a5962a477d4998ea19afe3cfc
Головнин Яков <yasha.golovnin@gmail.com>
2021-09-02 21:15:04 +0500

    Bug fixed reload city

--- src/components/LocationComponent.tsx
+++ src/components/LocationComponent.tsx
@@ -1,8 +1,9 @@
 import React, { FC } from 'react';
 import { useEffect } from 'react';
 import Geocode from "react-geocode";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { GOOGLE_MAPS_API_KEY } from '../constant';
+import { RootState } from '../store';
 import { setAlert } from '../store/actions/alertAction';
 import { setCity } from '../store/actions/cityAction';
 import { getWeather, setLoading } from '../store/actions/weatherActions';
@@ -11,29 +12,32 @@ Geocode.setApiKey(GOOGLE_MAPS_API_KEY);
 Geocode.enableDebug();
 
 const LocationComponent: FC = () => {
-    const dispatch = useDispatch();  
+    const dispatch = useDispatch();
+    const city: string = useSelector((state: RootState) => state.city.name);
 
     useEffect(() => {
-        navigator.geolocation.getCurrentPosition(position => {
-            Geocode.fromLatLng(position.coords.latitude, position.coords.longitude).then(
-                (response) => {
-                    const addressArray: any = response.results[0].address_components;
-                    const city = getCity(addressArray);
-
-                    if (city === undefined)
+        if (city === '') {
+            navigator.geolocation.getCurrentPosition(position => {
+                Geocode.fromLatLng(position.coords.latitude, position.coords.longitude).then(
+                    (response) => {
+                        const addressArray: any = response.results[0].address_components;
+                        const city = getCity(addressArray);
+    
+                        if (city === undefined)
+                            dispatch(setAlert('City ​​not found!'));
+                        else {
+                            dispatch(setCity(city));
+                            dispatch(setLoading());
+                            dispatch(getWeather(city));
+                        }
+                    },
+                    (error) => {
                         dispatch(setAlert('City ​​not found!'));
-                    else {
-                        dispatch(setCity(city));
-                        dispatch(setLoading());
-                        dispatch(getWeather(city));
+                        console.error(error);
                     }
-                },
-                (error) => {
-                    dispatch(setAlert('City ​​not found!'));
-                    console.error(error);
-                }
-            );
-        });
+                );
+            });
+        }
     }, []);
 
     const getCity = (addressArray) => {
